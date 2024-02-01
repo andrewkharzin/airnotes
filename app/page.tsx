40292717df19cd96880358b0276758a8f2638adc
Andrew Khz <andrewkhz@Andrews-iMac.local>
2024-02-01 15:36:07 +0300

    Main page title changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@nextui-org/link";
+import Image  from "next/image"
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
@@ -9,42 +10,23 @@ import { GithubIcon } from "@/components/icons";
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+			            <Image
+							priority
+							src="/Logo.svg"
+							height={200}
+							width={200}
+							alt="Follow us on Twitter"
+							/>
 			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+				<h1 className={title()}>Organize&nbsp;</h1>
+				<h1 className={title({ color: "violet" })}> your activites &nbsp;</h1>
 				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
+				<h1 className="mt-2 text-bold text-3xl text text-purpule-800">
+					productive and reliable with 
 				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
+				<h2 className="mt-5 font-mono uppercase text-4xl text-bold bg-gradient-to-r from-slate-200 to-fuchsia-800 text-transparent bg-clip-text">
+					airnotes
 				</h2>
-			</div>
-
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
 			</div>
 		</section>
 	);
